7e2178471a6c2a13b3223cc428da49090bae04fd
Oliver Maskery <oliver@wellplayed.games>
2020-06-06 16:25:15 +0100

    Add funcs to patch an object if patch isn't no-op

--- pkg/patch/patch.go
+++ pkg/patch/patch.go
@@ -1,6 +1,8 @@
 package patch
 
 import (
+	"context"
+	"fmt"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -13,4 +15,32 @@ func IsPatchRequired(newObj runtime.Object, patch client.Patch) (bool, error) {
 	}
 
 	return string(p) != "{}", nil
+}
+
+// MaybePatch will patch an object if the patch does not produce a no-op
+func MaybePatch(ctx context.Context, client client.Client, newObj runtime.Object, patch client.Patch) (bool, error) {
+	required, err := IsPatchRequired(newObj, patch)
+	if err != nil {
+		return false, fmt.Errorf("unable to build patch: %v", err)
+	}
+
+	if !required {
+		return false, nil
+	}
+
+	return true, client.Patch(ctx, newObj, patch)
+}
+
+// MaybePatchStatus will patch an object's status if the patch does not produce a no-op
+func MaybePatchStatus(ctx context.Context, client client.Client, newObj runtime.Object, patch client.Patch) (bool, error) {
+	required, err := IsPatchRequired(newObj, patch)
+	if err != nil {
+		return false, fmt.Errorf("unable to build patch: %v", err)
+	}
+
+	if !required {
+		return false, nil
+	}
+
+	return true, client.Status().Patch(ctx, newObj, patch)
 }
--- pkg/patch/patch_test.go
+++ pkg/patch/patch_test.go
@@ -1,6 +1,7 @@
 package patch
 
 import (
+	"context"
 	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -12,7 +13,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-type brokenPatcher struct {}
+type brokenPatcher struct{}
 
 func (b brokenPatcher) Type() types.PatchType {
 	return "broken"
@@ -24,28 +25,82 @@ func (b brokenPatcher) Data(obj runtime.Object) ([]byte, error) {
 
 var _ client.Patch = &brokenPatcher{}
 
+type fakeClientState struct {
+	patchCalled       bool
+	patchStatusCalled bool
+}
+
+type fakeClient struct {
+	state        *fakeClientState
+	statusWriter fakeStatusWriter
+}
+
+type fakeStatusWriter struct {
+	state *fakeClientState
+}
+
+func newFakeClient() fakeClient {
+	state := &fakeClientState{
+		patchCalled:       false,
+		patchStatusCalled: false,
+	}
+	return fakeClient{
+		state: state,
+		statusWriter: fakeStatusWriter{
+			state: state,
+		},
+	}
+}
+
+func (f fakeStatusWriter) Update(context.Context, runtime.Object, ...client.UpdateOption) error {
+	panic("unimplemented")
+}
+
+func (f *fakeStatusWriter) Patch(context.Context, runtime.Object, client.Patch, ...client.PatchOption) error {
+	f.state.patchStatusCalled = true
+	return nil
+}
+
+func (f fakeClient) Get(context.Context, client.ObjectKey, runtime.Object) error {
+	panic("unimplemented")
+}
+
+func (f fakeClient) List(context.Context, runtime.Object, ...client.ListOption) error {
+	panic("unimplemented")
+}
+
+func (f fakeClient) Create(context.Context, runtime.Object, ...client.CreateOption) error {
+	panic("unimplemented")
+}
+
+func (f fakeClient) Delete(context.Context, runtime.Object, ...client.DeleteOption) error {
+	panic("unimplemented")
+}
+
+func (f fakeClient) Update(context.Context, runtime.Object, ...client.UpdateOption) error {
+	panic("unimplemented")
+}
+
+func (f fakeClient) Patch(context.Context, runtime.Object, client.Patch, ...client.PatchOption) error {
+	f.state.patchCalled = true
+	return nil
+}
+
+func (f fakeClient) DeleteAllOf(context.Context, runtime.Object, ...client.DeleteAllOfOption) error {
+	panic("unimplemented")
+}
+
+func (f fakeClient) Status() client.StatusWriter {
+	return &f.statusWriter
+}
+
+var _ client.Client = &fakeClient{}
+
 var _ = Describe("IsPatchRequired", func() {
 	var originalObject *corev1.Service
 
 	BeforeEach(func() {
-		originalObject = &corev1.Service{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "test-service",
-				Namespace: "test-namespace",
-			},
-			Spec: corev1.ServiceSpec{
-				Ports: []corev1.ServicePort{
-					{
-						Name: "http",
-						Port: 80,
-					},
-				},
-				Selector: map[string]string{
-					"app":     "backend",
-					"version": "v1",
-				},
-			},
-		}
+		originalObject = makeTestService()
 	})
 
 	When("there is no difference", func() {
@@ -86,3 +141,133 @@ var _ = Describe("IsPatchRequired", func() {
 		})
 	})
 })
+
+var _ = Describe("MaybePatch", func() {
+	var originalObject *corev1.Service
+	var k8sClient fakeClient
+
+	BeforeEach(func() {
+		originalObject = makeTestService()
+		k8sClient = newFakeClient()
+	})
+
+	When("there is no difference", func() {
+		var unchangedObject *corev1.Service
+
+		BeforeEach(func() {
+			unchangedObject = originalObject.DeepCopy()
+		})
+
+		It("should not patch the object", func() {
+			patched, err := MaybePatch(context.Background(), k8sClient, unchangedObject, client.MergeFrom(originalObject))
+			Expect(err).To(Succeed(), "should not fail")
+			Expect(patched).To(BeFalse(), "should not patch an identical object")
+			Expect(k8sClient.state.patchCalled).To(BeFalse(), "should not have called patch")
+		})
+	})
+
+	When("there is a difference", func() {
+		var changedObject *corev1.Service
+
+		BeforeEach(func() {
+			changedObject = originalObject.DeepCopy()
+			changedObject.Labels = labels.Set{
+				"deployment": "best-deployment",
+			}
+		})
+
+		It("should patch the object", func() {
+			patched, err := MaybePatch(context.Background(), k8sClient, changedObject, client.MergeFrom(originalObject))
+			Expect(err).To(Succeed(), "should not fail")
+			Expect(patched).To(BeTrue(), "should patch a changed object")
+			Expect(k8sClient.state.patchCalled).To(BeTrue(), "should have called patch")
+		})
+	})
+
+	When("the patch fails to generate", func() {
+		It("should return an error", func() {
+			patched, err := MaybePatch(context.Background(), k8sClient, nil, brokenPatcher{})
+			Expect(err).ToNot(Succeed(), "should fail")
+			Expect(patched).To(BeFalse(), "should not patch if unable to generate patch")
+			Expect(k8sClient.state.patchCalled).To(BeFalse(), "should not have called patch")
+		})
+	})
+})
+
+var _ = Describe("MaybePatchStatus", func() {
+	var originalObject *corev1.Service
+	var k8sClient fakeClient
+
+	BeforeEach(func() {
+		originalObject = makeTestService()
+		k8sClient = newFakeClient()
+	})
+
+	When("there is no difference", func() {
+		var unchangedObject *corev1.Service
+
+		BeforeEach(func() {
+			unchangedObject = originalObject.DeepCopy()
+		})
+
+		It("should not patch the object", func() {
+			patched, err := MaybePatchStatus(context.Background(), k8sClient, unchangedObject, client.MergeFrom(originalObject))
+			Expect(err).To(Succeed(), "should not fail")
+			Expect(patched).To(BeFalse(), "should not patch an identical object")
+			Expect(k8sClient.state.patchStatusCalled).To(BeFalse(), "should not have called patch")
+		})
+	})
+
+	When("there is a difference", func() {
+		var changedObject *corev1.Service
+
+		BeforeEach(func() {
+			changedObject = originalObject.DeepCopy()
+			changedObject.Status.LoadBalancer = corev1.LoadBalancerStatus{
+				Ingress: []corev1.LoadBalancerIngress{
+					{
+						IP:       "1.2.3.4",
+						Hostname: "website.cool",
+					},
+				},
+			}
+		})
+
+		It("should patch the object", func() {
+			patched, err := MaybePatchStatus(context.Background(), k8sClient, changedObject, client.MergeFrom(originalObject))
+			Expect(err).To(Succeed(), "should not fail")
+			Expect(patched).To(BeTrue(), "should patch an changed object")
+			Expect(k8sClient.state.patchStatusCalled).To(BeTrue(), "should have called patch")
+		})
+	})
+
+	When("the patch fails to generate", func() {
+		It("should return an error", func() {
+			patched, err := MaybePatchStatus(context.Background(), k8sClient, nil, brokenPatcher{})
+			Expect(err).ToNot(Succeed(), "should fail")
+			Expect(patched).To(BeFalse(), "should not patch if unable to generate patch")
+			Expect(k8sClient.state.patchStatusCalled).To(BeFalse(), "should not have called patch")
+		})
+	})
+})
+
+func makeTestService() *corev1.Service {
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-service",
+			Namespace: "test-namespace",
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name: "http",
+					Port: 80,
+				},
+			},
+			Selector: map[string]string{
+				"app":     "backend",
+				"version": "v1",
+			},
+		},
+	}
+}
